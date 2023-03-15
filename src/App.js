03063a696f26908aed8f078bef56ad0139294e3f
Danish Shafi <danish.shafi@live.co.uk>
2023-03-15 17:05:31 +0000

    basic structure created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Faker from './components/Faker';
+import TweetBox from './components/TweetBox';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Vibely. All we do is send good vibes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Faker />
-      </header>
+      <TweetBox />
     </div>
   );
 }
---
+++ src/components/TweetBox.js
@@ -0,0 +1,47 @@
+import { IconButton, Box, Avatar, Card, CardActions, CardContent, Button, TextField } from '@mui/material';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import PhotoSizeSelectActualIcon from '@mui/icons-material/PhotoSizeSelectActual';
+import GifBoxIcon from '@mui/icons-material/GifBox';
+
+function TweetBox() {
+  return (
+    <Box
+      component="form"
+      autoComplete="off"
+    >
+      <Card sx={{ maxWidth: 550, height: 130 }}>
+
+        <CardContent className="row d-flex flex-nowrap justify-content-around">
+          <Avatar>D</Avatar>
+          <TextField
+            sx={{ maxWidth: 450 }}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+        </CardContent>
+
+        <CardActions className="row">
+          <div className="d-flex justify-content-between">
+            <div id="tweetIconRow" >
+              <IconButton><PhotoSizeSelectActualIcon/></IconButton>
+              <IconButton><GifBoxIcon/></IconButton>
+              <IconButton><EmojiEmotionsIcon /></IconButton>
+            </div>
+            <div id="tweetPostButton">
+              <Button
+                sx={{ borderRadius: 50 }}
+                variant="contained"
+                color="primary"
+                href="#contained-buttons">
+                Vibe
+              </Button>
+            </div>
+          </div>
+        </CardActions>
+
+      </Card>
+    </Box>
+  )
+}
+
+export default TweetBox
