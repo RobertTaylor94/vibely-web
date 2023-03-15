98b0c7342dcda362afc1f71951869cb6a14e6442
Robert Taylor <rjt903@gmail.com>
2023-03-15 14:27:01 +0000

    Created new user screen

---
+++ src/components/Authentication/signupScreen.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Button, Container, TextField, Box, Typography } from "@mui/material";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from '../../firebaseConfig'
+
+function SignUp() {
+    let auth = getAuth();
+    const [data, setData] = useState({});
+
+    const handleInput = (event) => {
+        let newInput = { [event.target.name]: event.target.value }
+        setData({ ...data, ...newInput})
+        console.log(data.email, data.password)
+    }
+
+    const handleSubmit = () => {
+        console.log("handleSubmit pressed")
+        createUserWithEmailAndPassword(auth, data.email, data.password)
+        .then((response) => {
+            console.log(response.user)
+        })
+        .catch((err) => {
+            alert(err.message)
+        })
+    }
+
+    return (
+    <div>
+        <Typography variant='h2' gutterBottom>Sign Up</Typography>
+        <Box component='form' sx={{ margin: '20px' }}>
+            <TextField
+                sx={{ margin: '10' }}
+                name='email'
+                id='email'
+                label='Email'
+                variant='outlined'
+                onChange={(e) => handleInput(e)}
+            />
+            <TextField
+                sx={{ margin: '10' }}
+                name='password'
+                id='password'
+                label='Password'
+                variant='outlined'
+                onChange={(e) => handleInput(e)}
+            />
+            <Box sx={{ margin: '10px' }}><Button type='submit' variant='contained' onClick={handleSubmit}> Submit</Button></Box>
+            </Box>
+    </div>
+            
+    )
+}
+
+export default SignUp
