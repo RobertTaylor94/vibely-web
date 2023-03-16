ae2150d1b7baf0d6604ba39f6439ebfeedd966e5
Robert Taylor <rjt903@gmail.com>
2023-03-16 16:12:02 +0000

    diplsay user email on profile screen

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,16 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
+import { useAuthValue } from "../AuthContext";
 
 export default function Profile() {
+    const {currentUser} = useAuthValue()
+    console.log(currentUser)
+    //set user email from currentUser if it is not null
+    const userEmail = currentUser ? currentUser.email : ''
+
     return (
-        <div>
-            Profile
-        </div>
+        <Container>
+            <Typography variant="h3">User: {userEmail}</Typography>
+        </Container>
     )
 }
