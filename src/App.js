0a66a4ee7297811e383e8d1cd72b7d6761b572d7
Robert Taylor <rjt903@gmail.com>
2023-03-18 12:44:47 +0000

    Fixed error with missing import

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { AuthProvider } from './AuthContext';
 import { auth } from './firebaseConfig';
 import { onAuthStateChanged } from 'firebase/auth';
 import SetupProfile from './pages/SetupProfile';
+import FollowerProfile  from './pages/FollowerProfile';
 
 function App() {
 
