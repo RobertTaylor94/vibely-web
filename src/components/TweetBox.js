19862134c41cbc61d097a5ff95752c699ac5e192
Robert Taylor <rjt903@gmail.com>
2023-03-18 12:50:11 +0000

    show currentUser inital in tweetbox avatar

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,25 +1,36 @@
-import { IconButton, Box, Avatar, Card, CardActions, CardContent, Button, TextField } from '@mui/material';
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
-import PhotoSizeSelectActualIcon from '@mui/icons-material/PhotoSizeSelectActual';
-import GifBoxIcon from '@mui/icons-material/GifBox';
+import {
+  IconButton,
+  Box,
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  TextField,
+} from "@mui/material";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import PhotoSizeSelectActualIcon from "@mui/icons-material/PhotoSizeSelectActual";
+import GifBoxIcon from "@mui/icons-material/GifBox";
+import { useAuthValue } from "../AuthContext";
 
 function TweetBox() {
+  const { currentUser } = useAuthValue();
+  let initial = "";
+  if (currentUser && currentUser.displayName) {
+    initial = currentUser.displayName.charAt(0).toUpperCase();
+  } else {
+    initial = "P";
+  }
 
-const postVibe = () => {
-
-}
+  const postVibe = () => {};
 
   return (
-    <Box
-      component="form"
-      autoComplete="off"
-    >
+    <Box component="form" autoComplete="off">
       <Card sx={{ height: 130 }}>
-
         <CardContent className="row d-flex flex-nowrap justify-content-around">
-          <Avatar>D</Avatar>
+          <Avatar>{initial}</Avatar>
           <TextField
-            sx={{ maxWidth: '85%' }}
+            sx={{ maxWidth: "85%" }}
             placeholder="What's on your mind?"
             variant="standard"
           />
@@ -27,26 +38,32 @@ const postVibe = () => {
 
         <CardActions className="row">
           <div className="d-flex justify-content-between">
-            <div id="tweetIconRow" >
-              <IconButton><PhotoSizeSelectActualIcon/></IconButton>
-              <IconButton><GifBoxIcon/></IconButton>
-              <IconButton><EmojiEmotionsIcon /></IconButton>
+            <div id="tweetIconRow">
+              <IconButton>
+                <PhotoSizeSelectActualIcon />
+              </IconButton>
+              <IconButton>
+                <GifBoxIcon />
+              </IconButton>
+              <IconButton>
+                <EmojiEmotionsIcon />
+              </IconButton>
             </div>
             <div id="tweetPostButton">
               <Button
                 sx={{ borderRadius: 50 }}
                 variant="contained"
                 color="primary"
-                onClick={(e) => postVibe(e)}>
+                onClick={(e) => postVibe(e)}
+              >
                 Vibe
               </Button>
             </div>
           </div>
         </CardActions>
-
       </Card>
     </Box>
-  )
+  );
 }
 
-export default TweetBox
+export default TweetBox;
