307beed55ff640c29c3587121b119dc8dad92fad
Robert Taylor <rjt903@gmail.com>
2023-03-22 16:07:23 +0000

    render postcard for each post from firestore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,62 @@
-import SideCard from './SideCard.js'
-import TweetBox from './TweetBox.js'
-import StoriesBar from './StoriesBar.js'
-import Postcard from './PostCard/PostCard.js'
-import NewsList from './NewsList.js'
+import SideCard from "./SideCard.js";
+import TweetBox from "./TweetBox.js";
+import StoriesBar from "./StoriesBar.js";
+import Postcard from "./PostCard/PostCard.js";
+import NewsList from "./NewsList.js";
+import { db } from "../firebaseConfig.js";
+import {
+  collectionGroup,
+  query,
+  getDocs,
+  collection,
+  onSnapshot,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    return (
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col">
-            <SideCard />
-          </div>
-          <div className="col-6">
-            <StoriesBar />
-            <TweetBox />
-            <div id='feed'>
-              <Postcard />
-            </div>
-          </div>
-          <div className="col">
-            <NewsList/>
+
+  const [posts, setPosts] = useState([]);
+
+  function postsSnapshot() {
+    const q = query(collectionGroup(db, "posts"));
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      QuerySnapshot.forEach((doc) => {
+        const newPost = {
+          "id": doc.id,
+          "data": doc.data()
+        }
+        console.log(newPost)
+        setPosts(posts => [...posts, newPost]);
+        console.log(posts)
+      });
+      console.log(`---------------------------------------`);
+    });
+  }
+
+  useEffect(() => {
+    postsSnapshot();
+  }, []);
+
+  const postsList = posts.map(post => <Postcard key={post.id} displayname={post.data.displayname} post={post.data.post}/>)
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <SideCard />
+        </div>
+        <div className="col-6">
+          <StoriesBar />
+          <TweetBox />
+          <div id="feed">
+            {postsList}
           </div>
         </div>
+        <div className="col">
+          <NewsList />
+        </div>
       </div>
-    )
+    </div>
+  );
 }
